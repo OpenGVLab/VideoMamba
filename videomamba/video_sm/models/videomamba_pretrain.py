49fcff77a68b72be8fa611dca06e6d404c8409e4
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2024-04-10 20:48:28 +0200

    Update videomamba/video_sm/models/videomamba_pretrain.py
    Co-authored-by: Lucain <lucainp@gmail.com>

--- videomamba/video_sm/models/videomamba_pretrain.py
+++ videomamba/video_sm/models/videomamba_pretrain.py
@@ -238,10 +238,13 @@ def get_sinusoid_encoding_table(n_position, d_hid):
     return  torch.tensor(sinusoid_table, dtype=torch.float, requires_grad=False).unsqueeze(0) 
 
 
-class VisionMamba(nn.Module, 
-                  library_name="video_mamba",
-                  repo_url="https://github.com/OpenGVLab/VideoMamba",
-                  tags=["video-classification"]):
+class VisionMamba(
+    nn.Module,
+    PyTorchModelHubMixin
+    library_name="video_mamba",
+    repo_url="https://github.com/OpenGVLab/VideoMamba",
+    tags=["video-classification"]
+):
     def __init__(
             self, 
             img_size=224, 
